0b651322300b4d26729713b64d1a18e1fbea7a20
Marat-idk <molodoy.dracon@gmail.com>
2023-04-29 21:25:36 +0300

    added custom gradient view

---
+++ AnimeApp/Views/GradientView.swift
@@ -0,0 +1,58 @@
+//
+//  GradientView.swift
+//  AnimeApp
+//
+//  Created by Marat on 24.04.2023.
+//
+
+import UIKit
+
+// MARK: - GradientView
+final class GradientView: UIView {
+    
+    private let colors: [UIColor]
+    private let gradientLayer: CAGradientLayer
+    
+    var locations: [NSNumber]? {
+        get {
+            gradientLayer.locations
+        }
+        set {
+            gradientLayer.locations = newValue
+        }
+    }
+    
+    var startPoint: CGPoint {
+        get {
+            gradientLayer.startPoint
+        }
+        set {
+            gradientLayer.startPoint = newValue
+        }
+    }
+    
+    var endPoint: CGPoint {
+        get {
+            gradientLayer.endPoint
+        }
+        set {
+            gradientLayer.endPoint = newValue
+        }
+    }
+    
+    init(colors: [UIColor]) {
+        self.colors = colors
+        self.gradientLayer = CAGradientLayer()
+        super.init(frame: .zero)
+        self.gradientLayer.colors = colors.map { $0.cgColor }
+        layer.addSublayer(gradientLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        gradientLayer.frame = bounds
+    }
+}
---
+++ AnimeApp/Views/MaterialTextField.swift
@@ -0,0 +1,100 @@
+//
+//  MaterialTextField.swift
+//  AnimeApp
+//
+//  Created by Marat on 24.04.2023.
+//
+
+import UIKit
+
+// MARK: - MaterialTextField
+class MaterialTextField: UITextField {
+    
+    // MARK: - public properies
+    var shouldReturnBlock: ((MaterialTextField) -> (Bool))?
+    var didBeginEditing: ((MaterialTextField) -> Void)?
+    var didEndEditing: ((MaterialTextField) -> Void)?
+    var editingChanged: ((MaterialTextField) -> Void)?
+
+    var placeholderInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    var textInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    var editingTextInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    
+    // MARK: - private properies
+    private weak var _delegate: UITextFieldDelegate?
+    
+    override var delegate: UITextFieldDelegate? {
+        get {
+            _delegate
+        }
+        set {
+            _delegate = newValue
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        super.delegate = self
+        addTargets()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: placeholderInset)
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: textInset)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: editingTextInset)
+    }
+    
+    private func addTargets() {
+//        addTarget(self, action: #selector(handleEditingDidBegin(_:)), for: .editingDidBegin)
+//        addTarget(self, action: #selector(handleEditingDidEnd(_:)), for: .editingDidEnd)
+        addTarget(self, action: #selector(handleEditingChanged(_:)), for: .editingChanged)
+    }
+    
+    @objc private func handleEditingDidBegin(_ sender: UITextField) {
+        didBeginEditing?(self)
+    }
+    
+    @objc private func handleEditingDidEnd(_ sender: UITextField) {
+        didEndEditing?(self)
+    }
+    
+    @objc private func handleEditingChanged(_ sender: UITextField) {
+        editingChanged?(self)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension MaterialTextField: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let outerDelegate = _delegate, outerDelegate.responds(to: #selector((textFieldShouldReturn(_:)))) {
+            outerDelegate.textFieldShouldReturn?(textField)
+        }
+        return shouldReturnBlock?(self) ?? true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if let outerDelegate = _delegate, outerDelegate.responds(to: #selector(textFieldDidBeginEditing(_:))) {
+            outerDelegate.textFieldDidBeginEditing?(textField)
+        }
+        
+        didBeginEditing?(self)
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let outerDelegate = _delegate, outerDelegate.responds(to: #selector(textFieldDidEndEditing(_:))) {
+            outerDelegate.textFieldDidEndEditing?(textField)
+        }
+        
+        didEndEditing?(self)
+    }
+}
