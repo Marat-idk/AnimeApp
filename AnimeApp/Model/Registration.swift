f7d87e14f345e342d6b836f0bd6f7cf8882013bc
Marat-idk <molodoy.dracon@gmail.com>
2023-05-02 07:47:06 +0300

    added registration model

---
+++ AnimeApp/Model/Registration.swift
@@ -0,0 +1,15 @@
+//
+//  Registration.swift
+//  AnimeApp
+//
+//  Created by Marat on 02.05.2023.
+//
+
+import Foundation
+
+// MARK: - Registration
+struct Registration {
+    var email: String?
+    var username: String?
+    var password: String?
+}
