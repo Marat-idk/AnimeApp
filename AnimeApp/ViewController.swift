6c1e35d283a1246e0e8b909c8aea512327923640
Marat-idk <molodoy.dracon@gmail.com>
2023-06-10 14:01:45 +0300

    draft commit

--- AnimeApp/ViewController.swift
+++ AnimeApp/ViewController.swift
@@ -9,23 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var networkManager = NetworkManagerImpl()
+    var userService = UserService.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
         
-        print(AuthorizationRequest.userCreate(email: "test@email.com", password: "1234.4321", username: "test").descriptionString)
+//        userService.login(email: "test69@email.com", and: "1234.4321") { [weak self] result in
+//            switch result {
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            case .success(let data):
+//                let session = data as? Session
+//                print(session)
+//            }
+//        }
         
-        networkManager.request(with: AuthorizationRequest.userCreate(email: "test@emt3fbg3fail.com", password: "1234.4321", username: "tes3gveb4t")) { data, response, error in
-            if let error = error {
+        userService.registration(email: "t24eest2@gmail.com", username: "test6625", password: "63tmy6.53") { [weak self] result in
+            
+            switch result {
+            case .failure(let error):
                 print(error.localizedDescription)
+            case .success(let data):
+                if let jsonData = data as? [String: Any], let error = jsonData["detail"] as? String {
+                    print(error)
+                }
             }
             
-            print((response as? HTTPURLResponse)?.statusCode)
-            
-            print(data?.count)
         }
     }
 }
