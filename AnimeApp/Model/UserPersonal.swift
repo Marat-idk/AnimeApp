2644ecabb51d4231d7a835fa5435b1935e2ea061
Marat-idk <molodoy.dracon@gmail.com>
2023-07-02 15:11:13 +0300

    added mock user model

---
+++ AnimeApp/Model/UserPersonal.swift
@@ -0,0 +1,17 @@
+//
+//  UserPersonal.swift
+//  AnimeApp
+//
+//  Created by Марат on 25.06.2023.
+//
+
+import Foundation
+
+// TODO: - must be finished
+// MARK: - UserPersonal
+struct UserPersonal {
+    var firstName: String = ""
+    var lastName: String? = ""
+    var email: String = ""
+    var phone: String = ""
+}
