31a07af1b3dd059fd08691463e12e8df0a139295
Marat-idk <molodoy.dracon@gmail.com>
2023-07-22 18:33:24 +0300

    helper for custom assets added

---
+++ AnimeApp/Helpers/UIImage+AnimeAppImages.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+AnimeAppImages.swift
+//  AnimeApp
+//
+//  Created by Марат on 21.07.2023.
+//
+
+import UIKit
+
+// MARK: - UIImage+AnimeAppImages
+extension UIImage {
+    // tab bar icons
+    static let home = UIImage(named: "ic-home")
+    static let search = UIImage(named: "ic-search")
+    static let download = UIImage(named: "ic-download")
+    static let personal = UIImage(named: "ic-person")
+}
