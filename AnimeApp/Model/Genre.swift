06d869708e8f544ba40b6e9ed279a70851b57e55
Marat-idk <molodoy.dracon@gmail.com>
2023-08-11 01:31:14 +0300

    added some features

--- AnimeApp/Model/Genre.swift
+++ AnimeApp/Model/Genre.swift
@@ -14,9 +14,18 @@ struct Genre: Mappable {
     var name: String?
     var url: String?
     var count: Int?
+    var isSelected: Bool? = false
     
     init?(map: Map) {
         mapping(map: map)
+    }
+    
+    init(malID: Int, name: String, url: String, count: Int, isSelected: Bool? = nil) {
+        self.malID = malID
+        self.name = name
+        self.url = url
+        self.count = count
+        self.isSelected = isSelected
     }
     
     mutating func mapping(map: Map) {
@@ -24,5 +33,23 @@ struct Genre: Mappable {
         name <- map["name"]
         url <- map["url"]
         count <- map["count"]
+    }
+    
+    // TODO: - hardcoded, should remove it maybe
+    static func topGenres() -> [Genre] {
+        return [
+            Genre(malID: -1, name: "All", url: "", count: -1, isSelected: true),
+            Genre(malID: 27, name: "Shounen", url: "https://myanimelist.net/anime/genre/27/Shounen", count: 1881),
+            Genre(malID: 4, name: "Comedy", url: "https://myanimelist.net/anime/genre/4/Comedy", count: 7156),
+            Genre(malID: 1, name: "Action", url: "https://myanimelist.net/anime/genre/1/Action", count: 4747),
+            Genre(malID: 24, name: "Sci-Fi", url: "https://myanimelist.net/anime/genre/24/Sci-Fi", count: 3109)
+        ]
+    }
+}
+
+// MARK: - Equatable
+extension Genre: Equatable {
+    static func == (lhs: Genre, rhs: Genre) -> Bool {
+        return lhs.malID == rhs.malID
     }
 }
