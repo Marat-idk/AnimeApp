8527f2257b623849221cf60490159cd5f5e55b66
Marat-idk <molodoy.dracon@gmail.com>
2023-10-21 12:54:14 +0300

    draft commit

--- AnimeApp/Modules/Home/View/Components/MainSection/SearchCollectionViewCell.swift
+++ AnimeApp/Modules/Home/View/Components/MainSection/SearchCollectionViewCell.swift
@@ -8,9 +8,16 @@
 import UIKit
 import SnapKit
 
+// MARK: - SearchCollectionViewCellDelegate
+protocol SearchCollectionViewCellDelegate: AnyObject {
+    func searchAnimes(_ searchText: String)
+}
+
 // MARK: - SearchCollectionViewCell
 final class SearchCollectionViewCell: UICollectionViewCell {
     static let identifier = String(describing: SearchCollectionViewCell.self)
+    
+    weak var delegate: SearchCollectionViewCellDelegate?
     
     private let containerView: UIView = {
         let view = UIView()
@@ -47,6 +54,38 @@ final class SearchCollectionViewCell: UICollectionViewCell {
         tf.delegate = self
         tf.layer.masksToBounds = true
         return tf
+    }()
+    
+    private lazy var searchStackView: UIStackView = {
+        let firstPlugView = UIView()
+        let secondPlugView = UIView()
+        
+        firstPlugView.snp.makeConstraints { $0.width.equalTo(16) }
+        secondPlugView.snp.makeConstraints { $0.width.equalTo(16) }
+        
+        let stack = UIStackView(arrangedSubviews:
+                                    [
+                                        firstPlugView,
+                                        searchImageView,
+                                        searchTextField,
+                                        filterContainerView,
+                                        secondPlugView
+                                    ])
+        stack.axis = .horizontal
+        stack.distribution = .fillProportionally
+        stack.alignment = .center
+        stack.spacing = 8
+        
+        stack.setCustomSpacing(0, after: firstPlugView)
+        stack.setCustomSpacing(0, after: filterContainerView)
+        
+        stack.backgroundColor = .brandBlue
+        stack.layer.cornerRadius = 20
+        stack.layer.cornerCurve = .continuous
+        stack.clipsToBounds = true
+        stack.layer.masksToBounds = true
+        
+        return stack
     }()
     
     private let filterContainerView: UIView = {
@@ -99,34 +138,20 @@ final class SearchCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupViews() {
-        contentView.addSubview(containerView)
-        
-        containerView.addSubviews(searchImageView, searchTextField, filterContainerView)
-        
+        contentView.addSubview(searchStackView)
         filterContainerView.addSubviews(filterSeparatorView, filterImageView)
     }
     
     private func setupConstraints() {
-        containerView.snp.makeConstraints {
+        searchImageView.snp.makeConstraints {
+            $0.size.equalTo(16)
+        }
+        
+        searchStackView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         
-        searchImageView.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.left.equalToSuperview().offset(16)
-            $0.size.equalTo(16)
-        }
-
-        searchTextField.snp.makeConstraints {
-            $0.verticalEdges.equalToSuperview()
-//            self.horizontalConstraints = make.horizontalEdges.equalToSuperview().inset(20).constraint
-            $0.leading.equalTo(searchImageView.snp.trailing).offset(8)
-            $0.trailing.equalTo(filterContainerView.snp.leading).offset(-8)
-        }
-        
         filterContainerView.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.right.equalToSuperview().offset(-16)
             $0.height.equalTo(16)
             $0.width.equalTo(25)
         }
@@ -150,8 +175,72 @@ final class SearchCollectionViewCell: UICollectionViewCell {
 }
 
 extension SearchCollectionViewCell: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        
+        DispatchQueue.main.asyncDeduped(target: self, after: 0.5) { [weak self] in
+            self?.delegate?.searchAnimes(text)
+        }
+    }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+}
+
+extension DispatchQueue {
+
+    /**
+     - parameters:
+        - target: Object used as the sentinel for de-duplication.
+        - delay: The time window for de-duplication to occur
+        - work: The work item to be invoked on the queue.
+     Performs work only once for the given target, given the time window. The last added work closure
+     is the work that will finally execute.
+     Note: This is currently only safe to call from the main thread.
+     Example usage:
+     ```
+     DispatchQueue.main.asyncDeduped(target: self, after: 1.0) { [weak self] in
+         self?.doTheWork()
+     }
+     ```
+     */
+    public func asyncDeduped(target: AnyObject, after delay: TimeInterval, execute work: @escaping @convention(block) () -> Void) {
+        let dedupeIdentifier = DispatchQueue.dedupeIdentifierFor(target)
+        if let existingWorkItem = DispatchQueue.workItems.removeValue(forKey: dedupeIdentifier) {
+            existingWorkItem.cancel()
+//            NSLog("Deduped work item: \(dedupeIdentifier)")
+        }
+        let workItem = DispatchWorkItem {
+            DispatchQueue.workItems.removeValue(forKey: dedupeIdentifier)
+
+            for ptr in DispatchQueue.weakTargets.allObjects {
+                if dedupeIdentifier == DispatchQueue.dedupeIdentifierFor(ptr as AnyObject) {
+                    work()
+//                    NSLog("Ran work item: \(dedupeIdentifier)")
+                    break
+                }
+            }
+        }
+
+        DispatchQueue.workItems[dedupeIdentifier] = workItem
+        DispatchQueue.weakTargets.addPointer(Unmanaged.passUnretained(target).toOpaque())
+
+        asyncAfter(deadline: .now() + delay, execute: workItem)
+    }
+
+}
+
+// MARK: - Static Properties for De-Duping
+private extension DispatchQueue {
+
+    static var workItems = [AnyHashable: DispatchWorkItem]()
+
+    static var weakTargets = NSPointerArray.weakObjects()
+
+    static func dedupeIdentifierFor(_ object: AnyObject) -> String {
+        return "\(Unmanaged.passUnretained(object).toOpaque())." + String(describing: object)
+    }
+
 }
