df7d35d4ba3a8848e7448f096deccc46aadb472f
Marat-idk <molodoy.dracon@gmail.com>
2023-10-21 13:35:20 +0300

    added search view, made classes drier

--- AnimeApp/Modules/Home/View/Components/MainSection/SearchCollectionViewCell.swift
+++ AnimeApp/Modules/Home/View/Components/MainSection/SearchCollectionViewCell.swift
@@ -19,103 +19,9 @@ final class SearchCollectionViewCell: UICollectionViewCell {
     
     weak var delegate: SearchCollectionViewCellDelegate?
     
-    private let containerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .brandBlue
-        view.layer.cornerRadius = 20
-        view.layer.cornerCurve = .continuous
-        view.clipsToBounds = true
-        view.layer.masksToBounds = true
-        return view
-    }()
-    
-    private let searchImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .brandBlue
-        imageView.isOpaque = true
-        imageView.image = .search?.template
-        imageView.tintColor = .brandGray
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        return imageView
-    }()
-    
-    private lazy var searchTextField: UITextField = {
-        let tf = UITextField()
-        tf.backgroundColor = .brandBlue
-        tf.font = .montserratMedium(size: 14)
-        tf.textColor = .white
-        tf.attributedPlaceholder = NSAttributedString(string: "Search a title...",
-                                                      attributes:
-                                                        [
-                                                        .foregroundColor: UIColor.brandGray
-                                                        ])
-        tf.clipsToBounds = true
-        tf.delegate = self
-        tf.layer.masksToBounds = true
-        return tf
-    }()
-    
-    private lazy var searchStackView: UIStackView = {
-        let firstPlugView = UIView()
-        let secondPlugView = UIView()
-        
-        firstPlugView.snp.makeConstraints { $0.width.equalTo(16) }
-        secondPlugView.snp.makeConstraints { $0.width.equalTo(16) }
-        
-        let stack = UIStackView(arrangedSubviews:
-                                    [
-                                        firstPlugView,
-                                        searchImageView,
-                                        searchTextField,
-                                        filterContainerView,
-                                        secondPlugView
-                                    ])
-        stack.axis = .horizontal
-        stack.distribution = .fillProportionally
-        stack.alignment = .center
-        stack.spacing = 8
-        
-        stack.setCustomSpacing(0, after: firstPlugView)
-        stack.setCustomSpacing(0, after: filterContainerView)
-        
-        stack.backgroundColor = .brandBlue
-        stack.layer.cornerRadius = 20
-        stack.layer.cornerCurve = .continuous
-        stack.clipsToBounds = true
-        stack.layer.masksToBounds = true
-        
-        return stack
-    }()
-    
-    private let filterContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .brandBlue
-        view.isOpaque = true
-        return view
-    }()
-    
-    private let filterSeparatorView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .brandGray
-        view.isOpaque = true
-        return view
-    }()
-    
-    private lazy var filterImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .brandBlue
-        imageView.isOpaque = true
-        imageView.image = .options
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        imageView.isUserInteractionEnabled = true
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(filterTapped(_:)))
-//        tapGesture.cancelsTouchesInView = false
-        imageView.addGestureRecognizer(tapGesture)
-        
-        return imageView
+    private lazy var searchView: SearchView = {
+        let searchView = SearchView(mode: .withFilter)
+        return searchView
     }()
     
     override init(frame: CGRect) {
@@ -138,39 +44,13 @@ final class SearchCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupViews() {
-        contentView.addSubview(searchStackView)
-        filterContainerView.addSubviews(filterSeparatorView, filterImageView)
+        contentView.addSubview(searchView)
     }
     
     private func setupConstraints() {
-        searchImageView.snp.makeConstraints {
-            $0.size.equalTo(16)
-        }
-        
-        searchStackView.snp.makeConstraints {
+        searchView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        
-        filterContainerView.snp.makeConstraints {
-            $0.height.equalTo(16)
-            $0.width.equalTo(25)
-        }
-        
-        filterSeparatorView.snp.makeConstraints {
-            $0.verticalEdges.equalToSuperview()
-            $0.left.equalToSuperview()
-            $0.width.equalTo(1)
-        }
-
-        filterImageView.snp.makeConstraints {
-            $0.verticalEdges.equalToSuperview()
-            $0.left.equalTo(filterSeparatorView.snp.right).offset(8)
-            $0.size.equalTo(16)
-        }
-    }
-    
-    @objc private func filterTapped(_ sender: UITapGestureRecognizer) {
-        print("filterTapped")
     }
 }
 
--- AnimeApp/Views/AnimesTableView.swift
+++ AnimeApp/Views/AnimesTableView.swift
@@ -35,76 +35,9 @@ final class AnimesTableView: UIView {
         }
     }
     
-    private let searchImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .brandBlue
-        imageView.isOpaque = true
-        imageView.image = .search?.template
-        imageView.tintColor = .brandGray
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        return imageView
-    }()
-    
-    private lazy var searchTextField: MaterialTextField = {
-        let tf = MaterialTextField()
-        tf.placeholderInset = .zero
-        tf.textInset = .zero
-        tf.editingTextInset = .zero
-        tf.backgroundColor = .brandBlue
-        tf.font = .montserratMedium(size: 14)
-        tf.textColor = .white
-        tf.attributedPlaceholder = NSAttributedString(string: "Search a title...",
-                                                      attributes:
-                                                        [
-                                                        .foregroundColor: UIColor.brandGray
-                                                        ])
-        tf.clipsToBounds = true
-        tf.delegate = self
-        tf.layer.masksToBounds = true
-        return tf
-    }()
-    
-    private lazy var searchStackView: UIStackView = {
-        let firstPlugView = UIView()
-        let secondPlugView = UIView()
-        
-        firstPlugView.snp.makeConstraints { $0.width.equalTo(16) }
-        secondPlugView.snp.makeConstraints { $0.width.equalTo(16) }
-        
-        let stack = UIStackView(arrangedSubviews:
-                                    [
-                                        firstPlugView,
-                                        searchImageView,
-                                        searchTextField,
-                                        secondPlugView
-                                    ])
-        stack.axis = .horizontal
-        stack.distribution = .fillProportionally
-        stack.alignment = .center
-        stack.spacing = 8
-        
-        stack.setCustomSpacing(0, after: firstPlugView)
-        stack.setCustomSpacing(0, after: searchTextField)
-        
-        stack.backgroundColor = .brandBlue
-        stack.layer.cornerRadius = 20
-        stack.layer.cornerCurve = .continuous
-        stack.clipsToBounds = true
-        stack.layer.masksToBounds = true
-        
-        return stack
-    }()
-    
-    private lazy var cancelButton: UIButton = {
-        let btn = UIButton()
-        btn.backgroundColor = .brandDarkBlue
-        btn.isOpaque = true
-        btn.setTitle("Cancel", for: .normal)
-        btn.setTitleColor(.white, for: .normal)
-        btn.setTitleColor(.brandWhiteGray, for: .highlighted)
-        btn.titleLabel?.font = .montserratMedium(size: 12)
-        return btn
+    private lazy var searchView: SearchView = {
+        let searchView = SearchView(mode: .withCancelButton)
+        return searchView
     }()
     
     private lazy var tableView: UITableView = {
@@ -154,8 +87,7 @@ final class AnimesTableView: UIView {
     private func setupViews() {
         addSubview(tableView)
         if mode == .search {
-            addSubviews(searchStackView,
-                        cancelButton)
+            addSubview(searchView)
         }
     }
     
@@ -166,30 +98,17 @@ final class AnimesTableView: UIView {
                 $0.edges.equalToSuperview()
             }
         case .search:
-            searchImageView.snp.makeConstraints {
-                $0.size.equalTo(Constants.SearchView.iconSize).priority(.required)
-            }
-
-            searchStackView.snp.makeConstraints {
+            searchView.snp.makeConstraints {
                 $0.top.equalToSuperview()
                 $0.leading.equalToSuperview().offset(24)
-                $0.height.equalTo(Constants.SearchView.height)
-                self.stackViewTrailingToSuperviewConstraint = $0.trailing.equalToSuperview().offset(-24).constraint
-                self.stackViewTrailingToCancelButtonConstraint = $0.trailing.equalTo(self.cancelButton.snp.leading).offset(-8).constraint
-            }
-            
-            cancelButton.snp.makeConstraints {
-                $0.centerY.equalTo(searchStackView)
-                $0.height.equalTo(Constants.CancelButton.height)
-                $0.width.equalTo(Constants.CancelButton.width)
-                self.cancelTrailingConstraint = $0.trailing.equalToSuperview().offset(Constants.CancelButton.hiddenTrailingOffset).constraint
+                $0.trailing.greaterThanOrEqualToSuperview().offset(-24)
+                $0.height.equalTo(40)
             }
             
             tableView.snp.makeConstraints {
-                $0.top.equalTo(searchStackView.snp.bottom).offset(24)
+                $0.top.equalTo(searchView.snp.bottom).offset(24)
                 $0.bottom.horizontalEdges.equalToSuperview()
             }
-            stackViewTrailingToCancelButtonConstraint?.deactivate()
         }
     }
     
@@ -199,53 +118,10 @@ final class AnimesTableView: UIView {
     }
 }
 
-extension AnimesTableView: UITextFieldDelegate {
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        layoutIfNeeded()
-        stackViewTrailingToSuperviewConstraint?.deactivate()
-        UIView.animate(withDuration: 0.1, delay: .zero, options: .curveLinear) {
-            self.cancelTrailingConstraint?.update(offset: Constants.CancelButton.showenTrailingOffset)
-            self.stackViewTrailingToCancelButtonConstraint?.activate()
-            self.layoutIfNeeded()
-        }
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        layoutIfNeeded()
-        stackViewTrailingToCancelButtonConstraint?.deactivate()
-        UIView.animate(withDuration: 0.1, delay: .zero, options: .curveLinear) {
-            self.cancelTrailingConstraint?.update(offset: Constants.CancelButton.hiddenTrailingOffset)
-            self.stackViewTrailingToSuperviewConstraint?.activate()
-            self.layoutIfNeeded()
-        }
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-}
-
 // MARK: - AnimesTableViewType
 extension AnimesTableView {
     enum DisplayMode {
         case normal
         case search
-    }
-}
-
-// MARK: - Constants
-fileprivate struct Constants {
-    
-    struct SearchView {
-        static let height = 40.0
-        static let iconSize = 16.0
-    }
-    
-    struct CancelButton {
-        static let height = 15.0
-        static let width  = 43.0
-        static let hiddenTrailingOffset = width
-        static let showenTrailingOffset = -8.0
     }
 }
