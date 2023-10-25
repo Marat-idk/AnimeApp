bb77522f0e7a89313eec20865142ae1c42dc48e1
Marat-idk <molodoy.dracon@gmail.com>
2023-10-26 00:35:25 +0300

    added debounce

--- AnimeApp/Helpers/DispatchQueue+Debounce.swift
+++ AnimeApp/Helpers/DispatchQueue+Debounce.swift
@@ -2,7 +2,60 @@
 //  DispatchQueue+Debounce.swift
 //  AnimeApp
 //
-//  Created by Марат Чобанов on 26.10.2023.
+//  Created by Марат on 26.10.2023.
 //
 
 import Foundation
+
+// MARK: - DispatchQueue+Debounce
+extension DispatchQueue {
+    /**
+     - parameters:
+        - target: Object used as the sentinel for de-duplication.
+        - delay: The time window for de-duplication to occur
+        - work: The work item to be invoked on the queue.
+     Performs work only once for the given target, given the time window. The last added work closure
+     is the work that will finally execute.
+     Note: This is currently only safe to call from the main thread.
+     Example usage:
+     ```
+     DispatchQueue.main.asyncDeduped(target: self, after: 1.0) { [weak self] in
+         self?.doTheWork()
+     }
+     ```
+     */
+    public func asyncDeduped(target: AnyObject, after delay: TimeInterval, execute work: @escaping @convention(block) () -> Void) {
+        let dedupeIdentifier = DispatchQueue.dedupeIdentifierFor(target)
+        if let existingWorkItem = DispatchQueue.workItems.removeValue(forKey: dedupeIdentifier) {
+            existingWorkItem.cancel()
+        }
+        let workItem = DispatchWorkItem {
+            DispatchQueue.workItems.removeValue(forKey: dedupeIdentifier)
+
+            for ptr in DispatchQueue.weakTargets.allObjects {
+                if dedupeIdentifier == DispatchQueue.dedupeIdentifierFor(ptr as AnyObject) {
+                    work()
+                    break
+                }
+            }
+        }
+
+        DispatchQueue.workItems[dedupeIdentifier] = workItem
+        DispatchQueue.weakTargets.addPointer(Unmanaged.passUnretained(target).toOpaque())
+        asyncAfter(deadline: .now() + delay, execute: workItem)
+    }
+
+}
+
+// MARK: - Static Properties for De-Duping
+private extension DispatchQueue {
+
+    static var workItems = [AnyHashable: DispatchWorkItem]()
+
+    static var weakTargets = NSPointerArray.weakObjects()
+
+    static func dedupeIdentifierFor(_ object: AnyObject) -> String {
+        return "\(Unmanaged.passUnretained(object).toOpaque())." + String(describing: object)
+    }
+
+}
